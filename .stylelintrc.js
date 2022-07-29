dbd6c8ca47049df41b81abf96d8c9f8d38ebf33c
zlogzr <zylogzr@163.com>
2022-07-29 17:17:26 +0800

    fix: 修复stylelint规则

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-idiomatic-order'],
   customSyntax: require('postcss-less'),
   rules: {
+    'string-quotes': 'double', // 双引号
     'no-empty-source': null,
     'number-leading-zero': 'never' // 小数不带0
   }
